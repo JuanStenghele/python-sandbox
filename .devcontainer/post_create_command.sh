62dce125a9d52a4c73852bc46c854bf44ec0b95d
Juan Stenghele <juan.stenghele@innovid.com>
2025-05-23 18:26:26 +0000

    [FIX] - Dependency injection session bug

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 # Install all Python dependencies
 pip install --upgrade --requirement /workspaces/example-api/requirements.txt --requirement /workspaces/example-api/test/requirements.txt
