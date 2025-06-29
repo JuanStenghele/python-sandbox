0db775114cbe27ce3a1b92de8152f62b7cfe0d85
Juan Stenghele <juanstenghele@gmail.com>
2025-06-29 21:07:53 +0000

    [FIX] - K8s stateful set

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,6 +36,21 @@ module "vpc" {
   }
 }
 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.39.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.5"
@@ -55,6 +70,12 @@ module "eks" {
       min_size       = 1
       max_size       = 3
       desired_size   = 2
+    }
+  }
+
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
     }
   }
 }
